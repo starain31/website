df3a00e2aa7ea2ada2bd1a1c7cb0d7c041e62e35
Nazmus Sakib <nsakib.cse@gmail.com>
2020-12-04 08:36:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const web = express();
 web.use(express.static('./public'));
 
 web.use('/', function (req, res) {
-    res.send({message: 'hi'})
+    res.send({message: 'Assalamualikum'})
 });
 
 web.listen(process.env.PORT || 3000);
